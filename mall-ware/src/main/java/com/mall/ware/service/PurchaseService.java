67f4a83531bfe1e4ac935daa186a8c1ebcce74f5
messengerW <messenger232624@gmail.com>
2020-11-25 00:23:47 +0800

    初级篇完毕

--- mall-ware/src/main/java/com/mall/ware/service/PurchaseService.java
+++ mall-ware/src/main/java/com/mall/ware/service/PurchaseService.java
@@ -3,7 +3,10 @@ package com.mall.ware.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.mall.common.utils.PageUtils;
 import com.mall.ware.entity.PurchaseEntity;
+import com.mall.ware.vo.MergeVo;
+import com.mall.ware.vo.PurchaseDoneVo;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,5 +19,13 @@ import java.util.Map;
 public interface PurchaseService extends IService<PurchaseEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    PageUtils queryPageUnreceivePurchase(Map<String, Object> params);
+
+    void mergePurchase(MergeVo mergeVo);
+
+    void received(List<Long> ids);
+
+    void done(PurchaseDoneVo doneVo);
 }
 
--- mall-ware/src/main/java/com/mall/ware/service/impl/WareInfoServiceImpl.java
+++ mall-ware/src/main/java/com/mall/ware/service/impl/WareInfoServiceImpl.java
@@ -11,6 +11,7 @@ import com.mall.common.utils.Query;
 import com.mall.ware.dao.WareInfoDao;
 import com.mall.ware.entity.WareInfoEntity;
 import com.mall.ware.service.WareInfoService;
+import org.springframework.util.StringUtils;
 
 
 @Service("wareInfoService")
@@ -18,9 +19,21 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        QueryWrapper<WareInfoEntity> queryWrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+
+        if ( !StringUtils.isEmpty(key) ) {
+            // 如果搜索框输入值不为空，进行模糊查询
+            queryWrapper.eq("id", key)
+                    .or().like("name", key)
+                    .or().like("address", key)
+                    .or().like("areacode", key);
+        }
+
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
